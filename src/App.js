903ce0bb347881ca76678e1986a5bd396264915b
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-03 16:38:14 +0530

    Demo DRUD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [movieName, setMovieName] = useState();
+  const [review, setReview] = useState();
+
+  const submitReview = () => {
+    Axios.post("http://localhost:5000/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      alert("successfull insert")
+    }).catch((err) => {
+      alert("feild to insert");
+      console.log(err);
+    })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD-Application🤩</h1>
+
+      <div className="form">
+        <label>Movie Name🎦 </label>
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
+        <label>Review🚮 </label>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        />
+
+        <button type="button" onClick={submitReview}>Submit</button>
+      </div>
     </div>
   );
 }
